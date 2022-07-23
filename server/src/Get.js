3607fce3487b3408df10ea38df71859eb6bcdeec
XecK <admin@xeck.fr>
2022-07-24 00:13:00 +0200

    fix

--- server/src/Get.js
+++ server/src/Get.js
@@ -3,7 +3,6 @@ var shared = require(process.cwd()+'/public/js/shared/Shared.js');
 
 
 exports.exportModifiedCityTile = async (options) => {
-
 	if(shared.checkOptions(options, ['city_id'])) {
 		let rows = await db.query(`
 			SELECT tile_x, tile_y, building_type_id, building_level, building_rotation, terrain_type_id,health
@@ -14,15 +13,15 @@ exports.exportModifiedCityTile = async (options) => {
 		rows.forEach(function(row) {
 			let line = {
 				x: row.tile_x,
-				y: row.tyle_y,
+				y: row.tile_y,
 				health: row.health
 			};
-			if (line.building_type_id) {
+			if (row.building_type_id) {
 				line.b = row.building_type_id;
 				line.b_l = row.building_level;
 				line.b_r = row.building_rotation;
 			}
-			if (line.terrain_type_id) {
+			if (row.terrain_type_id) {
 				line.t = row.terrain_type_id;
 			}
 			result.push(line);
