e9e63d1e8abaf38b033cf023549256a2f031c42c
Kilorbine <thomas.grockowiak@gmail.com>
2022-07-23 20:24:41 +0200

    ajout fichier de test front

--- public/js/test.js
+++ public/js/test.js
@@ -4,8 +4,8 @@ const socket = new WebSocket('ws://localhost:8081');
 // La connexion est ouverte
 socket.addEventListener('open', function (event) {
 	var data = {
-		action: 'BUILD TAVER',
-		options: '3'
+		action: 'CONNECT',
+		options: {user:'test', password:'test'}
 	};
 socket.send(JSON.stringify(data));
 });
@@ -14,3 +14,19 @@ socket.send(JSON.stringify(data));
 socket.addEventListener('message', function (event) {
   console.log('Voici un message du serveur', event.data);
 });
+
+function build_taverne() {
+	console.log('coucou');
+	var data = {
+		action: 'BUILD',
+		options: {
+			city_id:1,
+			type:2
+			x:5
+			y:7
+			rotation:0
+		}
+	};
+	socket.send(JSON.stringify(data));
+}
+
