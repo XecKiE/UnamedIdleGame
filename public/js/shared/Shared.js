5b4f260750ece721c3f8f94a033593fd0fc6a06f
thomas grockowiak <t.grockowiak@spartoo.com>
2022-07-29 10:16:03 +0200

    creation de troupes fonctionnelles

--- public/js/shared/Shared.js
+++ public/js/shared/Shared.js
@@ -102,6 +102,8 @@ export {
 	CBR,
 	economyProduction,
 	Unit,
+	UnitR,
+	UnitPrice,
 }
 
 
--- server/src/Armies.js
+++ server/src/Armies.js
@@ -9,15 +9,16 @@ export const updateCityArmy = async (options) => {
 export const recrutUnit = async (options) => {
 	//je pense qu'il faut un building_id pour atacher une unit_queue a un building
 	if (shared.checkOptions(options, ['city_id', 'unit_type'])) {
-		let ressource = economy.updateRessources({city_id:options.city_id});
+
+		let ressource = await economy.updateRessources({city_id:options.city_id});
 		if (ressource !== false &&
-			ressource.iq >= shared.unitPrice[shared.Unit[options.unit_type]].iron &&
-			ressource.wq >= shared.unitPrice[shared.Unit[options.unit_type]].wood &&
-			ressource.gq >= shared.unitPrice[shared.Unit[options.unit_type]].gold
+			ressource.iq >= shared.UnitPrice[shared.UnitR[options.unit_type]].iron &&
+			ressource.wq >= shared.UnitPrice[shared.UnitR[options.unit_type]].wood &&
+			ressource.gq >= shared.UnitPrice[shared.UnitR[options.unit_type]].gold
 		) {
 			let ins_unit = await db.query(`
 				INSERT INTO units(unit_type)
-				VALUES(${db.int(shared.Unit[options.unit_type])})
+				VALUES(${db.int(shared.UnitR[options.unit_type])})
 			`);
 			if (ins_unit !== [] && ins_unit.affectedRows === 1) {
 				ins_unit.insertId
@@ -27,7 +28,7 @@ export const recrutUnit = async (options) => {
 					WHERE city_id = ${db.int(options.city_id)}
 				`);
 				if (city_info.length == 1) {
-					city_info = army_id[0];
+					city_info = city_info[0];
 					await db.query(`
 						INSERT INTO armies_unit(army_id, unit_id)
 						VALUES(${db.int(city_info.army_id)}, ${db.int(ins_unit.insertId)})
@@ -35,15 +36,17 @@ export const recrutUnit = async (options) => {
 					await db.query(`
 						UPDATE ressources
 						SET
-							iron_quantity = iron_quantity - ${shared.unitPrice[shared.Unit[options.unit_type]].iron},
-							wood_quantity = wood_quantity - ${shared.unitPrice[shared.Unit[options.unit_type]].wood},
-							gold_quantity = gold_quantity - ${shared.unitPrice[shared.Unit[options.unit_type]].gold},
-						WHERE ressource_id = ${db.int(line.ressource_id)}
+							iron_quantity = iron_quantity - ${shared.UnitPrice[shared.UnitR[options.unit_type]].iron},
+							wood_quantity = wood_quantity - ${shared.UnitPrice[shared.UnitR[options.unit_type]].wood},
+							gold_quantity = gold_quantity - ${shared.UnitPrice[shared.UnitR[options.unit_type]].gold}
+						WHERE ressource_id = ${db.int(city_info.ressource_id)}
 					`);
 					return {
-						iq:(ressource.iq - shared.unitPrice[shared.Unit[options.unit_type]].iron),
-						wq:(ressource.wq - shared.unitPrice[shared.Unit[options.unit_type]].wood),
-						gq:(ressource.gq - shared.unitPrice[shared.Unit[options.unit_type]].gold),
+						data : {
+							iq:(ressource.iq - shared.UnitPrice[shared.UnitR[options.unit_type]].iron),
+							wq:(ressource.wq - shared.UnitPrice[shared.UnitR[options.unit_type]].wood),
+							gq:(ressource.gq - shared.UnitPrice[shared.UnitR[options.unit_type]].gold),							
+						}
 					};
 				}
 			}
